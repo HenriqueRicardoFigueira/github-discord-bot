0399a1ea1e1832b5cbb083fd9a6a37cafa591e43
HenriqueRicardoFigueira <henrislip@gmail.com>
2024-07-19 16:21:39 -0300

    add webhook controller

--- app/controllers/webhook_controller.rb
+++
@@ -1,2 +0,0 @@
-class WebhookController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  post "webhooks/github" => "webhooks#github"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
