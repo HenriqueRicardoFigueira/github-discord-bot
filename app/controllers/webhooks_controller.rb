3548746bb9deb98a56d158699eed2fd0dc01ee8e
HenriqueRicardoFigueira <henrislip@gmail.com>
2024-07-27 10:42:05 -0300

    adjustments on code

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -6,29 +6,32 @@ class WebhooksController < ApplicationController
 
   def github
     event = request.headers['X-GitHub-Event']
-    payload = JSON.parse(request.body.read)
+    @payload = JSON.parse(request.body.read)
 
-    send_message(payload)
+    send_message
 
     head :ok
   end
 
   private 
 
-  def send_message(payload)
+  def send_message
     bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN']
+    message = define_mesasge
+    bot.send_message(ENV['DISCORD_CHANNEL_ID'], message)
+  end
 
-    message = case payload['action']
-              when 'released'
-                "New release: #{payload['release']['tag_name']}, Author: #{payload['release']['author']['login']} #{payload['release']['body']}}"
-              when 'labeled'
-                if payload['label']['name'].include?('code review')
-                  "PR ##{payload['pull_request']['number']} - #{payload['pull_request']['title']} has been labeled for code review"
-                end
-              when 'published'
-                "New release: #{payload['release']['tag_name']}, Author: #{payload['release']['author']['login']} #{payload['release']['body']}}"
-              end
-    bot.send_message(DISCORD_CHANNEL_ID, message)
+  def define_mesasge
+    case @payload['action']
+    when 'released'
+      "New release: #{@payload['release']['tag_name']}, Author: #{@payload['release']['author']['login']} #{@payload['release']['body']}}"
+    when 'labeled'
+      if @payload['label']['name'].include?('code review')
+        "PR ##{@payload['pull_request']['number']} - #{@payload['pull_request']['title']} has been labeled for code review"
+      end
+    when 'published'
+      "New release: #{@payload['release']['tag_name']}, Author: #{@payload['release']['author']['login']} #{@payload['release']['body']}}"
+    end
   end
 
 end
