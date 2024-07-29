2ffdceccf4ad9f949f1b630cb3f1ed5c3858ba0e
HenriqueRicardoFigueira <henrislip@gmail.com>
2024-07-29 16:58:16 -0300

    remove create message from controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -5,45 +5,8 @@ class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def github
-    event = request.headers['X-GitHub-Event']
-    @payload = JSON.parse(request.body.read)
-
-    send_message
-
+    MessageService.new(payload).send_message
     head :ok
-  end
-
-  private 
-
-  def send_message
-    bot = Discordrb::Bot.new token: ENV['DISCORD_BOT_TOKEN']
-    message = define_mesasge
-    bot.send_message(ENV['DISCORD_CHANNEL_ID'], message)
-  end
-
-  def define_mesasge
-    case @payload['action']
-    when 'released'
-      released
-    when 'labeled'
-      labeled
-    when 'published'
-      published
-    end
-  end
-
-  def published
-    "New release: #{@payload['release']['tag_name']}, Author: #{@payload['release']['author']['login']} #{@payload['release']['body']}}"
-  end
-
-  def labeled
-    return unless !@payload['label']['name'].include?('code review')
-    
-    "PR ##{@payload['pull_request']['number']} - #{@payload['pull_request']['title']} has been labeled for code review"
-  end
-
-  def released
-    "New release: #{@payload['release']['tag_name']}, Author: #{@payload['release']['author']['login']} #{@payload['release']['body']}}"
   end
 
 end
